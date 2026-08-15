ab57f7f1f812fd08a191d0ee2b6c095b7f199e5f
EMMANUEL OKUNLOLA <74790322+emmy16-glitch@users.noreply.github.com>
2026-08-15 02:23:49 +0100

    Load focused homepage layout corrections

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './styles/tokens.css'
 import './styles/global.css'
 import App from './App.tsx'
+import './styles/erp-pos-layout-fixes.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
