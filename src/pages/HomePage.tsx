c61d2e8e61d03651844daa59eb1532dc67e1144b
EMMANUEL OKUNLOLA <74790322+emmy16-glitch@users.noreply.github.com>
2026-08-18 05:56:32 +0100

    Load homepage vector visual system

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -4,6 +4,7 @@ import {
   HomeReferenceLightSections,
 } from '../sections/home/HomeReferenceContent'
 import { HomeReferenceHero } from '../sections/home/HomeReferenceHero'
+import '../styles/home-visual-system-v2.css'
 
 export function HomePage() {
   return (
